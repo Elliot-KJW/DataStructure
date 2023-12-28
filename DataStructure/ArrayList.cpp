07b0885b6ae68d78d6aceb1f9d2d2f5d534756e3
Elliot-KJW <eidechse4@hanmail.net>
2023-12-28 23:34:24 +0900

    자료구조 2일차 정적 ArrayList 함수 추가 구현

--- DataStructure/ArrayList.cpp
+++ DataStructure/ArrayList.cpp
@@ -1,4 +1,6 @@
 #include "ArrayList.hpp"
+#include <iostream>
+#include <algorithm>
 #include <cassert>
 
 template <typename T>
@@ -14,6 +16,34 @@ void ArrayList<T>::insert(T newEntry) {
 }
 
 template <typename T>
+void ArrayList<T>::remove(T target) {
+	assert(used > 0);
+
+	for (int i = 0; i < used; i++) {
+		if (data[i] == target) {
+			data[i] = data[--used];
+			break;
+		}
+	}
+}
+
+template <typename T>
+void ArrayList<T>::operator +=(const ArrayList<T>& addend) {
+	assert(used + addend.size() <= CAPACITY);
+
+	std::copy(addend.data, addend.data + addend.size(), data + used);
+	used += addend.size();
+}
+
+template <typename T>
 size_t ArrayList<T>::size() const {
 	return this->used;
+}
+
+template <typename T>
+void ArrayList<T>::print() const{
+	for(size_t i = 0; i < used; i++) {
+		std::cout << data[i] << " ";
+	}
+	printf("\n");
 }
--- DataStructure/main.cpp
+++ DataStructure/main.cpp
@@ -2,9 +2,19 @@
 
 int main() {
 	ArrayList<int> list = ArrayList<int>();
+	ArrayList<int> list2 = ArrayList<int>();
 	list.insert(4);
 	list.insert(8);
 	list.insert(4);
+
+	list.print();
+
+	list2.insert(5);
+	list2.insert(7);
+
+	list += list2;
+
+	list.print();
 
 	return 0;
 }
