390c0f506ad0b684b75f49bae4f5497cf663c64f
Elliot-KJW <eidechse4@hanmail.net>
2023-12-28 23:34:24 +0900

    자료구조 2일차 정적 ArrayList 함수 추가 구현

--- DataStructure/ArrayList.hpp
+++ DataStructure/ArrayList.hpp
@@ -7,11 +7,21 @@ public:
 	void insert(T newEntry);
 	// precondition: used < CAPACITY
 	// postcondition: new Entry inserted in list
+
 	void remove(T target);
+	// precondition: used > 0
+	// postcondition: remove one Entry equal to target 
+
+	void operator +=(const ArrayList<T>& addend);
+
 	size_t size() const;
 	// postcondition: return used 
-	size_t occurences(T target) const;
 
+	size_t occurences(T target) const;
+	// postcondition: retunr num of target number in data 
+
+	void print() const;
+	// postcondition: print list data
 public:
 	static const size_t CAPACITY = 20;
 
