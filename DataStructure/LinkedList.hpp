7380221e36f5153d9e767134e5ae4343adee2a74
Elliot-KJW <eidechse4@hanmail.net>
2024-01-11 15:16:30 +0900

    자료구조 6일차 node 연산 추가

--- DataStructure/LinkedList.hpp
+++ DataStructure/LinkedList.hpp
@@ -1,29 +1,10 @@
-#pragma once
+#include "Node.cpp"
 #include "List.hpp"
-#include <cstdlib>
 
-class node {
-public:
-	typedef double value_type;
+template <typename T>
+class LinkedList : List<T> {
 private:
-	value_type data_filed;
-	// 다음 노드를 가리키는 포인터
-	node* link_filed;
-
-public:
-	node(const value_type& init_data = value_type(), const node* init_link = NULL) {
-		this->data_filed = init_data;
-		this->link_filed = (node*)init_link;
-	}
-	void set_data(const value_type& new_data);
-	void set_link(node* new_link);
-	value_type data() const;
-	const node* link() const;
-	node* link();
-
+	Node<T>* head_ptr;
+	// 마지막 노드를 가리킴
+	Node<T>* tail_ptr;
 };
-
-// 첫번째 노드를 가리킴
-node* head_ptr;
-// 마지막 노드를 가리킴
-node* tail_ptr;
